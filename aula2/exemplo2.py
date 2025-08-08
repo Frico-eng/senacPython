66871f9f91176ac77613afe67c184a9537c6e684
Frederico Lopes <frederico59699816@edu.pa.senac.br>
2025-08-08 08:22:25 -0300

    initial commit

---
+++ aula2/exemplo2.py
@@ -0,0 +1,7 @@
+idade = 20
+salario = 5000.5
+altura = 1.73
+genero = "f"
+nome = "maria silva"
+
+print(f"\n\nA funcionária: {nome}\nsexo: {genero},\naltura: {altura:.2f} cm,\nsalario: R${salario:.2f} reais e tem {idade} anos\n\n")
