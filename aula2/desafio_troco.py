8e2e280f2e6f54256720f3351158f55efef14dc7
frico <fhrl.eng@gmail.com>
2025-08-08 08:44:05 -0300

    testing removal

--- aula2/desafio_troco.py
+++
@@ -1,7 +0,0 @@
-preco_unit = float(input("insira o preço unitário do produto: "))
-unidades = int(input("insira quantas unidades foram compradas: "))
-pago = float(input("insira o dinheiro recebido: "))
-
-troco = pago-(preco_unit*unidades)
-
-print(f"\n\nPara a compra de R$ {preco_unit*unidades:.2f} com pagamento de R$ {pago:.2f} o seu troco será de: R$ {troco:.2f}\n\n")
