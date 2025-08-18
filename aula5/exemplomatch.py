ae01f648bcc371f59e3b26b32eac91e5c1c8b7f2
frico <fhrl.eng@gmail.com>
2025-08-18 09:12:30 -0300

    Add aula4 as a regular folder

--- aula5/exemplomatch.py
+++ aula5/exemplomatch.py
@@ -1,1 +1,15 @@
 opcao = int(input("Escolha uma operação:\n1 - Soma\n2 - Subtração\n3 - multiplicação\n4 - divisão\n------------------\n"))
+num1 = float(input("\nnInsira o primeiro número:"))
+num2 = float(input("\nInsira o segundo número:"))
+
+match opcao:
+    case 1:
+        print(f"Resultado da soma: {num1+num2}")
+    case 2:
+        print(f"Resultado da subtração: {num1-num2}")
+    case 3:
+        print(f"Resultado da soma: {num1*num2}")
+    case 4:
+        print(f"Resultado da soma: {num1/num2}")
+    case _:
+        print(f"Opeção invalida")
